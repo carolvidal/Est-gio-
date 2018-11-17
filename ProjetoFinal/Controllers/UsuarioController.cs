88fa13c13c6b9ea11dd6eb4bce598477b3ad68ac
Carol Vidal <carolvidal931@gmail.com>
2018-11-17 11:47:19 -0200

    TAKE 192188292202010

---
+++ ProjetoFinal/Controllers/UsuarioController.cs
@@ -0,0 +1,76 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ProjetoFinal.Models;
+
+namespace ProjetoFinal.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        [HttpGet]
+        public IActionResult Login(int? id)
+        {
+            if (id != null)
+            {
+                if (id == 0)
+                {
+                    HttpContext.Session.SetString("IdUsuarioLogado", string.Empty);
+                    HttpContext.Session.SetString("NomeUsuarioLogado", string.Empty);
+
+                }
+            }
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Autenticar(UsuarioModel usuario)
+        {
+            bool login = usuario.Autenticar();
+            if (login)
+            {
+                HttpContext.Session.SetString("NomeUsuarioLogado", usuario.Nome);
+                HttpContext.Session.SetString("IdUsuarioLogado", usuario.Id.ToString());
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                TempData["MensagemLoginInvalido"] = "Usuario ou senha inválidos!";
+                return RedirectToAction("Login");
+            }
+        }
+
+        [HttpGet]
+        public IActionResult NovoUsuario()
+        {
+            return View();
+
+        }
+
+        [HttpPost]
+        public IActionResult Registrar(UsuarioModel usuario)
+        {
+
+            bool cadastro = usuario.Registrar();
+
+            if (cadastro)
+            {
+                TempData["MensagemCadastroSucesso"] = "Usuario cadastrado com sucesso!";
+                return RedirectToAction("NovoUsuario");
+            }
+            else
+            {
+                TempData["MensagemCadastroInvalido"] = "Usuario não cadastrado";
+                return RedirectToAction("NovoUsuario");
+            }
+        }
+        public IActionResult CadastroUsuario()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ ProjetoFinal/Models/UsuarioModel.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data;
+using ProjetoFinal.Util;
+
+namespace ProjetoFinal.Models
+{
+    public class UsuarioModel
+    {
+
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public string Senha { get; set; }
+        public DateTime Data_Nascimento { get; set; }
+
+        public bool Autenticar()
+        {
+            string sql = $"SELECT ID, NOME, DATA_NASCIMENTO FROM CADASTRO WHERE EMAIL='{Email}'" +
+                $" AND SENHA='{Senha}'";
+            DAL objDAO = new DAL();
+            DataTable dt = objDAO.RetDataTable(sql);
+
+            if (dt != null)
+            {
+                if (dt.Rows.Count == 1)
+                {
+                    Id = int.Parse(dt.Rows[0]["ID"].ToString()); //dados para sessao
+                    Nome = dt.Rows[0]["NOME"].ToString(); //dados para sessao
+                    Data_Nascimento = DateTime.Parse(dt.Rows[0]["DATA_NASCIMENTO"].ToString());
+                    return true;
+                }
+            }
+            return false;
+        }
+
+
+        public bool Cadastrar()
+        {
+
+
+
+            string sql = $"INSERT INTO CADASTRO(NOME,EMAIL,SENHA,DATA_NASCIMENTO) VALUES ('{Nome}','{Email}','{Senha}','{Data_Nascimento}')";
+            DAL objDAL = new DAL();
+            objDAL.ExecutarComandoSql(sql);
+            return true;
+
+
+
+
+        }
+    }
+}
+    
